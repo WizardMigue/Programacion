53d5e06a4883a898e52ff26aace55bb07ef05562
WizardMigue <208609@iessierradeguara.com>
2022-05-08 22:35:30 +0200

    Salucionado Ejercicio 17

--- Ud1/src/Ejercicios/Ejercicio17.java
+++ Ud1/src/Ejercicios/Ejercicio17.java
@@ -6,37 +6,22 @@ public class Ejercicio17 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
-		double cantidadPrestada, interesFijo, interesVariable, cuota, cantidadTotal;
-		int anyosPrestamo, numeroPagos;
-		
-		
-		System.out.print("Introduce la cantidad del prestamo, el interés anual "
-				+ "y duración del prestamo en años, separado por espacios: ");
-		cantidadPrestada = s.nextDouble();
-		interesFijo = s.nextDouble();
-		anyosPrestamo = s.nextInt();
-		
-		interesVariable = interesFijo / 100 / 12;
-		numeroPagos = anyosPrestamo * 12;
-		
-		
-		cuota = (cantidadPrestada * interesVariable) / (1-(1/((Math.pow(1+interesVariable,numeroPagos)))));
-		cantidadTotal = cuota * numeroPagos;
-		
-		System.out.println("Introduccion de datos para el calculo");		
-		System.out.println("Cantidad prestada: " + cantidadPrestada);		
-		System.out.println("Interes anual (%): " + interesFijo);		
-		System.out.println("Duracion en anyos del prestamo: " + anyosPrestamo);
-		System.out.println("\n");			
-		System.out.println("========================================");
-		System.out.println("Resultado de la operacion:");
-		System.out.println("Cantidad prestada:" + cantidadPrestada);
-		System.out.println("Interes anual:" + interesFijo + ("%"));
-		System.out.println("Duracion en anyos del prestamo: " + anyosPrestamo);
-		System.out.println("Numero de pagos: " + (anyosPrestamo * 12));
-		System.out.printf("Pago mensual: %1.2f", cuota );
-		System.out.printf("\nCantidad total a pagar: %1.2f euros", cantidadTotal);
-	}
-}
+		System.out.println("Cantidad prestada: ");
+		double x = s.nextDouble();
+		System.out.println("Interés anual: ");
+		double y = s.nextDouble();
+		System.out.println("Duracion del prestamo en años: ");
+		int z = s.nextInt();
+		double intMes = y / 100 / 12;
+		double pagoMes = ((x * intMes) / (1 - (1 / (double) (Math.pow((1 + intMes), (z * 12))))));
+		System.out.println("RESULTADO DE LA OPERACION:  ");
+		System.out.println("Cantidad prestada:  " + x + "Euros");
+		System.out.println("Interés anual: " + y + "%");
+		System.out.println("Duración en años del préstamo " + z);
+		System.out.println("Número de pagos: " + (z * 12));
+		System.out.printf("Pago mensual: %.2f \n", pagoMes);
+		System.out.printf("Cantidad total a pagar: %.2f Euros  ", (pagoMes * (z * 12)));
 
+	}
+
+}
